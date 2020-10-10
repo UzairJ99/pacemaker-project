013d87bc68f6975adb3970151e601109a9e698a3
Labeeb Hasan Zaker <labeebzaker@gmail.com>
2020-10-10 14:29:21 -0700

    made changes

---
+++ src/components/RegisterPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from '../images/logo.png';
+
+const Register = () =>{
+  return(
+    <div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Welcome to Register Page
+                </p>
+            </header>
+    </div>
+  )
+}
+export default Register
