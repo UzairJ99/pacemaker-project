47a1061b83f285675f1c1dbd6422ac5f2d264bbc
UzairJ99 <uzair-jawaid@hotmail.com>
2020-10-11 05:18:52 -0400

    created basic navbar and blank page for user interface

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import logo from '../images/logo.png';
 // stylesheets
 import '../stylesheets/App.css';
+// components
+import NavbarComponent from './interface/NavbarComponent';
 
 const MainPage = () => {
     return (
         <div>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    All functionality (buttons, inputs, etc) go on this page.
-                </p>
-            </header>
+            <NavbarComponent />
+            <h1>System Dashboard</h1>
+            <p>
+                All functionality (buttons, inputs, etc) go on this page.
+            </p>
         </div>
     );
 }
