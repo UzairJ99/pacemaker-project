6f6a9cf57c179756bcbe3cda7d53f1d65374d8a2
Uzair99 <=>
2020-10-30 17:45:17 -0400

    check user authorization for redirect

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
-} from 'react-router-dom';
+  Route,
+  Redirect
+} from "react-router-dom";
 // stylesheets
 import '../stylesheets/App.css';
 // constants
@@ -13,8 +14,10 @@ import MainPage from './MainPage.js';
 import LandingPage from './LandingPage.js';
 import LoginPage from './LoginPage';
 import RegisterPage from './RegisterPage';
+// authorization
+import { withFirebase } from './Firebase/index';
 
-function App() {
+function App(props) {
   /* Controller for page rendering.
     React Router uses the Switch cases to determine which route was reached.
     @path = the url for the page to be rendered. constant values are saved in
@@ -22,12 +25,28 @@ function App() {
     inside of Route is the component to be rendered.
     The landing page route should always be the last route listed.
   */
+
+  // session handling
+  const [authUser, setAuthUser] = React.useState(null);
+
+  // check for change in global state
+  useEffect(() => {
+    props.firebase.auth.onAuthStateChanged(authUser => {
+      authUser
+        ? setAuthUser({ authUser })
+        : setAuthUser({ authUser: null });
+    });
+  });
+
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route path={ROUTES.MAIN}>
-            <MainPage />
+            {
+              // renders the page whether the user has logged in or not - otherwise redirect to login page
+              (authUser != null) ? <MainPage authUser={authUser} /> : <Redirect to={ROUTES.LOGIN} /> 
+            }
           </Route>
           <Route path={ROUTES.LOGIN}>
             <LoginPage />
