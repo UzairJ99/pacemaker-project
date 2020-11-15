1c7b30bf3d6bf923679504b46b18de7f073a2dd0
Labeeb Hasan Zaker <labeebzaker@gmail.com>
2020-11-14 16:42:26 -0800

    added server file

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -63,4 +63,4 @@ function App(props) {
   );
 }
 
-export default App;
+export default withFirebase(App);
