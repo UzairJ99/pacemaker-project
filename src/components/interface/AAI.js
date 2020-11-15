dd75842ce6721c655357c41c60ac66cd9ff8b546
Uzair99 <=>
2020-11-14 21:32:46 -0500

    adjusted from step 3 of assign 2

--- src/components/interface/AAI.js
+++ src/components/interface/AAI.js
@@ -11,9 +11,9 @@ const AAI = () => {
             <h5>Upper Rate Limit (ppm)</h5>
             <input type='number' min='50' max='175' step ='5'></input>
             <h5>Atrial Amplitude (V)</h5>
-            <input type='number' min='0' max='5' step='1.25'></input>
+            <input type='number' min='0' max='5' step='0.1'></input>
             <h5>Atrial Pulse Width (ms)</h5>
-            <input type='number' min='0.05' max='1.9' step='0.1'></input>
+            <input type='number' min='0.1' max='30' step='0.1'></input>
             <h5>Atrial Sensitivity (mV)</h5>
             <input type='number' min='0.25' max='10' step='0.25'></input>
             <h5>ARP</h5>
