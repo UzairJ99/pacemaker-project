d6aee3d58ffb268885236432eb635cfedf7de067
Nish Shah <shahnish2000@gmail.com>
2020-11-19 01:34:11 +0400

    Modified Serial port with path

--- backend/serialcomm.js
+++ backend/serialcomm.js
@@ -1,7 +1,12 @@
 var serialport = require("serialport"); 
-var SerialPort = serialport.SerialPort; 
 
-var serialPort = new SerialPort("COM4", {
-  baudrate: 115200,
-  parser: serialport.parsers.readline("\n")
+
+var serialPort = new serialport("USB\VID_1366&PID_1015&MI_00\6&80004E7&0&0000", {
+  baudRate: 115200,
 });
+serialPort.write('main screen turn on', function(err) {
+  if (err) {
+    return console.log('Error on write: ', err.message)
+  }
+  console.log('message written')
+})
