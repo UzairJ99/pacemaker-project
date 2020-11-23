81fcbef777cdde468542b758d4b9965c548adee7
Nish Shah <shahnish2000@gmail.com>
2020-11-24 01:07:44 +0400

    Buffer setup with Read/Write LED TRIAL

--- backend/serialcomm.js
+++ backend/serialcomm.js
@@ -1,32 +1,45 @@
 var serialport = require('serialport');
+const { database } = require('firebase-functions/lib/providers/firestore');
 var Readline = serialport.parsers.Readline;
 
 var port = new serialport('COM4',{
-  baudRate: 9600,
-  parser: new Readline("\r\n")
+  baudRate: 115200,
+  //parser: new Readline("\r\n")
 })
-
+var buffer = Buffer.alloc(5);
+buffer[0] = 0x16; //TO CHECK BEGININNG OF DATA
+//buffer[1] = 0x55; //FOR WRITING
+buffer[1] = 0x22; //FOR READING FROM SIMULINK/BOARD
+buffer[2] = 1; //RED
+buffer[3] = 0; //GREEN LED
+buffer[4] = 0; //BLUE
 // Pipe the data into another stream (like a parser or standard out)
 var lineStream = port.pipe(new Readline());
 
-port.write('main screen turn on', function(err) {
+port.write(buffer, function(err) {
   if (err) {
     return console.log('Error on write: ', err.message)
   }
-  console.log('message written')
+  console.log("message written");
 })
 
 
-port.on('readable', function (data) {
-  port.read() 
-})
-// Switches the port into "flowing mode"
-// port.on('data', function (data) {
-  
+
+
+// port.on('readable', function (data) {
+//   port.read();
+//   //console.log(port.read()); 
 // })
+//Switches the port into "flowing mode"
+if(buffer[1]==0x22){
+port.on('data', function (data) {
+  console.log(data);
+  })
+}
 lineStream.on('data', function (data) {
   console.log('Data:', data.toString('utf8'));
 });
+
 
 
 
