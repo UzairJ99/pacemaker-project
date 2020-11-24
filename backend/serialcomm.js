c627a10e81ddad800cb5fbe6878224c1f7381dc4
Labeeb Hasan Zaker <labeebzaker@gmail.com>
2020-11-23 18:32:12 -0800

    sending bunch of 1 byte parameters to the frdm board

--- backend/serialcomm.js
+++ backend/serialcomm.js
@@ -1,27 +1,94 @@
 var serialport = require('serialport');
-const { database } = require('firebase-functions/lib/providers/firestore');
+var express = require('express'); // used tosetup routes for backend processing
+var cors = require('cors');
+// const { database } = require('firebase-functions/lib/providers/firestore');
 var Readline = serialport.parsers.Readline;
+var bodyParser = require('body-parser');
+var buffer = Buffer.alloc(5);
 
-var port = new serialport('COM4',{
+
+/*
+Express routing handles packing and sending the data to the 
+pacemaker using post requests.  The data is taken from
+the PaceMode.js form, packaged, and then the writeToPort function 
+is called to actually write to the port.
+*/
+var app = express();
+var mode = 0;
+var currentMode = 0;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const expressPort = 8080;
+
+app.post('/writeToPort', (req, res) => {
+  // set the variables from the post requet
+  currentMode = req.body.modeVal.mode
+  console.log(currentMode);
+  
+  switch(currentMode){
+    case 'VOO':
+      mode = 1;
+      break;
+    case 'AOO':
+      mode = 2;
+      break;
+    case 'VVI':
+      mode = 3;
+      break;
+    case 'AAI':
+      mode = 4;
+      break;
+    case 'DOO':
+      mode = 5;
+      break;
+    case 'AOOR':
+      mode = 6;
+      break;
+    default:
+      mode = 1;
+  }
+
+  // package data here
+  buffer[0] = 0x16; //TO CHECK BEGININNG OF DATA
+  buffer[1] = 0x55; //FOR WRITING
+  //buffer[1] = 0x22; //FOR READING FROM SIMULINK/BOARD  
+  buffer[2] = mode; //RED
+  buffer[3] = 0; //GREEN LED
+  buffer[4] = 0; //BLUE
+
+  console.log(buffer);
+
+  //writeToPort(buffer); // uncomment this when done testing API calls
+})
+
+// initialize express port
+app.listen(expressPort, process.env.IP, () => {
+  console.log(`back end express server started on port: ${expressPort}`);
+});
+
+// SERIAL COMMUNICATION
+var port = new serialport('COM7',{
   baudRate: 115200,
   //parser: new Readline("\r\n")
 })
-var buffer = Buffer.alloc(5);
-buffer[0] = 0x16; //TO CHECK BEGININNG OF DATA
-//buffer[1] = 0x55; //FOR WRITING
-buffer[1] = 0x22; //FOR READING FROM SIMULINK/BOARD
-buffer[2] = 1; //RED
-buffer[3] = 0; //GREEN LED
-buffer[4] = 0; //BLUE
+
+
+
 // Pipe the data into another stream (like a parser or standard out)
 var lineStream = port.pipe(new Readline());
 
-port.write(buffer, function(err) {
-  if (err) {
-    return console.log('Error on write: ', err.message)
-  }
-  console.log("message written");
-})
+const writeToPort = (buffer) => {
+  port.write(buffer, function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message)
+    }
+    console.log("message written");
+  })
+}
+
 
 
 
