797583c9317e186d586714d8c8b523f196e1dfe5
Labeeb Hasan Zaker <labeebzaker@gmail.com>
2020-11-24 16:15:31 -0800

    made changes

--- src/components/interface/PaceMode.js
+++ src/components/interface/PaceMode.js
@@ -2,7 +2,7 @@
 This navbar component will let the user select which pacing mode interface to display on the page.
 It will control rendering the view
 */
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../../stylesheets/PaceMode.css';
 import Button from 'react-bootstrap/Button';
 // components
@@ -20,7 +20,27 @@ import DOOR from './DOOR';
 const PaceMode = () => {
 
     const [mode, setMode] = React.useState('VOO');
-
+    
+    /*
+    make call to backend API writeToPort POST request.
+    Pass in the state into the body in JSON format.
+    Request is processed in serialcomm.js and information is 
+    then sent to the FRDM.
+    */
+    useEffect(() => {
+        fetch('http://localhost:8080/writeToPort', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                modeVal: {mode}, // state has to be wrapped in curly braces to send properly
+            }),
+        })
+        .catch((err) => console.log(err))
+    }, [mode])
+    
+    // determine which mode to display on the UI
     function renderInterface (modeVal) {
         switch(modeVal) {
             case 'VOO': 
